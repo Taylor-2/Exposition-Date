c62ec3a41eecd9e1c66aa86cd0dba82dfb4fc921
Dexhubb <hubbade1@gmail.com>
2017-10-03 21:29:42 -0400

    Got submit working

--- Date Ideas V.2/Run.py
+++ Date Ideas V.2/Run.py
@@ -19,7 +19,7 @@ like ----> Date Ideas V.2
 					High
 					Free
 					Icon
-It's a shit file explanation, but you get my idea, I just don't know how to call them when they're like that
+It's a shit file explanation, but you get my idea, I just don't know how to call them in the code when they're like that
 
 '''
 window=Tk()
@@ -28,29 +28,29 @@ window.title("Date Ideas")
 #window.geometry("250x100")
 
 def submit():								#Function that runs every whenever you hit the submit button
-	vari=ideaEvar.get()						#Goal is to get it to take the value of the idea field, and the price/effort field
-	#filename=								#Then write the idea to the correct price text file
-	if (vari != "") and (vari != " "):
-		file=open("Idea List.txt","a")		#Currently only writes to the single "Idea List.txt" file
-		file.write(str(vari)+"\n")
-		file.close()
+	idea=ideaEvar.get()						#Goal is to get it to take the value of the idea field, and the price/effort field
+	filename=price.get()
+	print (filename)								#Then write the idea to the correct price text file
+	if (idea != "") and (idea != " "):
+		writer(filename,idea)
 		ideaEvar.set("")
+		price.set("Price/Effort")
 	else:
 		pass
 
 def random():										#Picks the first set of randomness, picks the number that determines what the
 	randomone=randint(0,100)						#Price/effort will be
 	if (randomone>=0 and randomone<=53):
-		#final=function("Free.txt")					#Currently works but isn't fully set up, just need to attach the correct files to the statements
+		#final=pickerpt2("Free.txt")					#Currently works but isn't fully set up, just need to attach the correct files to the statements
 		print("free")
 	elif (randomone>53 and randomone<=79):
-		#final=function("Low.txt")
-		print("low")						#These print statements are placeholders until we get the function(): hooked up
+		#final=pickerpt2("Low.txt")
+		print("low")						#These print statements are placeholders until we get the pickerpt2(): hooked up
 	elif (randomone>79 and randomone<=92):
-		#final=function("Med.txt")
+		#final=pickerpt2("Med.txt")
 		print("med")
 	elif (randomone>92 and randomone<=98):
-		#final=function("High.txt")
+		#final=pickerpt2("High.txt")
 		print("high")
 	elif (randomone==99):
 		print ("egg 1")
@@ -59,13 +59,18 @@ def random():										#Picks the first set of randomness, picks the number that
 	#print (final)
 	#Whatever I need to put here so that it's displayed as a label
 
-def function(filename):							#Function to take the price/effort filename from random(): then open the file, and choose an idea
+def writer(filename,idea):
+	file=open(filename + ".txt","a")
+	file.write(idea + "\n")
+	file.close()
+
+def pickerpt2(filename):						#Function to take the price/effort filename from random(): then open the file, and choose an idea
 	file=open(filename,"r")						#Second set of randomness
 	data=file.readlines()		
 	file.close()								#Currently works in theory, but hasn't been tested
 	leng=len(data)
 	number=randint(0,leng-1)
-	chosen=data[number]							#Still accepting better names than the function function
+	chosen=data[number]
 	leng=len(chosen)
 	chosen=str(chosen[0:leng-1])
 	return chosen
@@ -75,9 +80,9 @@ def delete():									#Function that will eventually delete an idea on a buttonc
 
 def dates():
 	pass 							#Lets make sure to put comments because I wanted to create a function
-									#that takes the variable created from random() then continues filtering it
+									#that takes the ideaable created from random() then continues filtering it
 
-ideaEvar=StringVar()			#Initializes variables for the tkinter
+ideaEvar=StringVar()			#Initializes ideaables for the tkinter
 ideaEvar.set("")
 randidea=StringVar()
 randidea.set("")
@@ -92,7 +97,7 @@ menu=OptionMenu(window, price, "Free ($0)", "Low ($5-$10)", "Medium ($10-$25)", 
 ideaL=Label(window,text="Idea: ")																		#idea label --> "Idea: " top left of the GUI
 ideaE=Entry(window,textvariable=ideaEvar,width=20)														#Idea Entery field
 butt=Button(window,text="Submit",command=submit)														#Submit Button
-randE=Entry(window,text=randidea)																		#Random Entery field --> I want this to be a label in the final version, but im not sure why it dosent work, if you want to mess around with it just change the "Entery" after the variable to "Label"
+randE=Entry(window,text=randidea)																		#Random Entery field --> I want this to be a label in the final version, but im not sure why it dosent work, if you want to mess around with it just change the "Entery" after the ideaable to "Label"
 randButt=Button(window,text="Random",command=random)													#Random Button
 randL=Label(window,text="Random: ")																		#Random Label --> "Random: "    Unnessecary once we get the random entery field to be a label
 delbutt=Button(window,text="Delete Idea",command=delete)												#Delete Button
